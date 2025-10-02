reportportal/service-api

Initial TMS migration

src/main/java/com/epam/ta/reportportal/core/tms/db/model/DataSet.java
@@ -1,0 +1,10 @@
+package com.epam.ta.reportportal.core.tms.db.model;
+
+import java.util.List;
+
+public class DataSet {
+    private Long id;
+    private String name;
+    private List<DataSetColumn> columns;
+    
+}

src/main/java/com/epam/ta/reportportal/core/tms/db/model/DataSetColumn.java
@@ -1,0 +1,7 @@
+package com.epam.ta.reportportal.core.tms.db.model;
+
+public class DataSetColumn {
+    private Long id;
+    private String key;
+    private String value;
+}

src/main/java/com/epam/ta/reportportal/core/tms/db/repository/TestFolderRepository.java
@@ -1,0 +1,30 @@
+package com.epam.ta.reportportal.core.tms.db.repository;
+
+import com.epam.ta.reportportal.core.tms.db.model.TestFolder;
+import com.epam.ta.reportportal.dao.ReportPortalRepository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Andrei_Varabyeu
+ */
+@Repository
+public interface TestFolderRepository extends ReportPortalRepository<TestFolder, Long> {
+    /**
+     * Find all folder for given project
+     *
+     * @param projectID ID of project
+     * @return found folders
+     */
+    List<TestFolder> findAllByProjectId(long projectID);
+
+    /**
+     * Finds a folder by given ID and project ID
+     * @param id ID of folder
+     * @param projectId ID of project
+     * @return Test Folder
+     */
+    TestFolder findByIdAndProjectId(long id, long projectId);
+}

src/main/java/com/epam/ta/reportportal/core/tms/mapper/ProductVersionMapper.java
@@ -1,0 +1,19 @@
+package com.epam.ta.reportportal.core.tms.mapper;
+
+import com.epam.ta.reportportal.core.tms.db.model.ProductVersion;
+import com.epam.ta.reportportal.core.tms.dto.ProductVersionRS;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductVersionMapper implements DtoMapper<ProductVersion, ProductVersionRS> {
+
+    @Override
+    public ProductVersionRS convert(ProductVersion productVersion) {
+        return new ProductVersionRS(productVersion.getId(),
+                productVersion.getVersion(),
+                productVersion.getDocumentation(),
+                productVersion.getTestPlans(),
+                productVersion.getMilestones());
+
+    }
+}

src/main/java/com/epam/ta/reportportal/core/tms/mapper/TestCaseMapper.java
@@ -1,0 +1,27 @@
+package com.epam.ta.reportportal.core.tms.mapper;
+
+import com.epam.ta.reportportal.core.tms.db.model.TestCase;
+import com.epam.ta.reportportal.core.tms.dto.TestCaseRS;
+import com.epam.ta.reportportal.core.tms.dto.TestCaseVersionRS;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class TestCaseMapper implements DtoMapper<TestCase, TestCaseRS> {
+
+    public TestCaseRS convert(final TestCase testCase) {
+        return new TestCaseRS(testCase.getId(),
+                testCase.getName(),
+                testCase.getDescription(),
+                testCase.getVersions()
+                        .stream()
+                        .map(model -> new TestCaseVersionRS(model.getId(),
+                                model.getName(),
+                                model.isDefault(),
+                                model.isDraft(),
+                                null))
+                        .collect(Collectors.toSet()));
+    }
+
+}

src/main/java/com/epam/ta/reportportal/core/tms/mapper/TestFolderMapper.java
@@ -1,0 +1,13 @@
+package com.epam.ta.reportportal.core.tms.mapper;
+
+import com.epam.ta.reportportal.core.tms.db.model.TestFolder;
+import com.epam.ta.reportportal.core.tms.dto.TestFolderRS;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestFolderMapper implements DtoMapper<TestFolder, TestFolderRS> {
+
+    public TestFolderRS convert(final TestFolder testFolder) {
+        return new TestFolderRS(testFolder.getId(), testFolder.getName(), testFolder.getDescription());
+    }
+}

src/main/java/com/epam/ta/reportportal/core/tms/service/ProductVersionServiceImpl.java
@@ -1,0 +1,76 @@
+package com.epam.ta.reportportal.core.tms.service;
+
+import com.epam.ta.reportportal.core.tms.db.model.Milestone;
+import com.epam.ta.reportportal.core.tms.db.model.ProductVersion;
+import com.epam.ta.reportportal.core.tms.db.model.TestPlan;
+import com.epam.ta.reportportal.core.tms.db.repository.ProductVersionRepository;
+import com.epam.ta.reportportal.core.tms.dto.ProductVersionRQ;
+import com.epam.ta.reportportal.core.tms.dto.ProductVersionRS;
+import com.epam.ta.reportportal.core.tms.exception.NotFoundException;
+import com.epam.ta.reportportal.core.tms.mapper.DtoMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductVersionServiceImpl implements ProductVersionService {
+
+    private static final String VERSION_NOT_FOUND_BY_ID = "Product Version cannot be found by id: {0}";
+
+    private final DtoMapper<ProductVersion, ProductVersionRS> productVersionMapper;
+    private final ProductVersionRepository productVersionRepository;
+
+    @Override
+    public ProductVersionRS create(long projectID, final ProductVersionRQ inputDto) {
+//        final var testSuite = testSuiteRepository.findById(inputDto.testFolderId())
+//                .orElseThrow(NotFoundException.supplier(TEST_SUITE_NOT_FOUND_BY_ID, inputDto.testFolderId())); // replace by getting default Test Suite
+        final var productVersion = new ProductVersion(null,
+                inputDto.version(),
+                inputDto.documentation(),
+                inputDto.testPlans().stream().map(it -> {
+                  var tp = new TestPlan();
+                  tp.setId(it);
+                  return tp;
+                }).collect(Collectors.toSet()),
+                inputDto.milestones().stream().map(it -> {
+                    var milestone = new Milestone();
+                    milestone.setId(it);
+                    return milestone;
+                }).collect(Collectors.toSet()));
+
+        return productVersionMapper.convert(productVersionRepository.save(productVersion));
+    }
+
+    @Override
+    public ProductVersionRS update(long projectID,final Long productVersionID, final ProductVersionRQ inputDto) {
+        final var productVersion = new ProductVersion(productVersionID,
+                inputDto.version(),
+                inputDto.documentation(),
+                inputDto.testPlans().stream().map(it -> {
+                    var tp = new TestPlan();
+                    tp.setId(it);
+                    return tp;
+                }).collect(Collectors.toSet()),
+                inputDto.milestones().stream().map(it -> {
+                    var milestone = new Milestone();
+                    milestone.setId(it);
+                    return milestone;
+                }).collect(Collectors.toSet()));
+
+        return productVersionMapper.convert(productVersionRepository.save(productVersion));    }
+
+    @Override
+    public void delete(long projectID,Long id) {
+        productVersionRepository.deleteById(id);
+    }
+
+    @Override
+    public ProductVersionRS getById(long projectID, Long id) {
+        return productVersionMapper.convert(productVersionRepository.findById(id)
+                .orElseThrow(NotFoundException.supplier(VERSION_NOT_FOUND_BY_ID, id)));
+    }
+
+
+}

src/main/java/com/epam/ta/reportportal/core/tms/service/TestCaseServiceImpl.java
@@ -1,0 +1,61 @@
+package com.epam.ta.reportportal.core.tms.service;
+
+import com.epam.ta.reportportal.core.tms.db.model.TestCase;
+import com.epam.ta.reportportal.core.tms.db.model.TestCaseVersion;
+import com.epam.ta.reportportal.core.tms.db.repository.TestCaseRepository;
+import com.epam.ta.reportportal.core.tms.db.repository.TestFolderRepository;
+import com.epam.ta.reportportal.core.tms.dto.TestCaseRQ;
+import com.epam.ta.reportportal.core.tms.dto.TestCaseRS;
+import com.epam.ta.reportportal.core.tms.exception.NotFoundException;
+import com.epam.ta.reportportal.core.tms.mapper.DtoMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static com.epam.ta.reportportal.core.tms.service.TestFolderServiceImpl.TEST_FOLDER_NOT_FOUND_BY_ID;
+
+@Service
+@RequiredArgsConstructor
+public class TestCaseServiceImpl implements TestCaseService {
+
+    private static final String TEST_CASE_NOT_FOUND_BY_ID = "Test Case cannot be found by id: {0}";
+
+    private final DtoMapper<TestCase, TestCaseRS> testCaseMapper;
+    private final TestCaseRepository testCaseRepository;
+    private final TestFolderRepository testFolderRepository;
+
+    @Override
+    public TestCaseRS createTestCase(final TestCaseRQ inputDto) {
+        final var testFolder = testFolderRepository.findById(inputDto.testFolderId())
+                .orElseThrow(NotFoundException.supplier(TEST_FOLDER_NOT_FOUND_BY_ID, inputDto.testFolderId())); // replace by getting default Test Folder
+        final var testCase = new TestCase(null,
+                inputDto.name(),
+                inputDto.description(),
+                new HashSet<>(),
+                new HashSet<>(),
+                testFolder);
+        testCase.addTestCaseVersion(new TestCaseVersion(null, "Default", true, false, null));
+
+        return testCaseMapper.convert(testCaseRepository.save(testCase));
+    }
+
+    @Override
+    public TestCaseRS updateTestCase(final long testCaseId, final TestCaseRQ inputDto) {
+        final var testCase = testCaseRepository.findById(testCaseId)
+                .orElseThrow(NotFoundException.supplier(TEST_FOLDER_NOT_FOUND_BY_ID, testCaseId)); // replace by getting default Test Folder
+        return null;
+    }
+
+    @Override
+    public TestCaseRS getTestCaseById(long projectId, long id) {
+        return testCaseMapper.convert(testCaseRepository.findById(id)
+                .orElseThrow(NotFoundException.supplier(TEST_CASE_NOT_FOUND_BY_ID, id)));
+    }
+    @Override
+    public List<TestCaseRS> getTestCaseByProjectId(long projectId) {
+        return testCaseRepository.findByTestFolder_ProjectId(projectId).stream().map(testCaseMapper::convert).toList();
+    }
+
+}

gleclaire/reportportal-service-api

src/main/java/com/epam/ta/reportportal/core/tms/db/entity/DataSet.java
@@ -1,0 +1,10 @@
+package com.epam.ta.reportportal.core.tms.db.entity;
+
+import java.util.List;
+
+public class DataSet {
+    private Long id;
+    private String name;
+    private List<DataSetColumn> columns;
+    
+}

src/main/java/com/epam/ta/reportportal/core/tms/db/entity/DataSetColumn.java
@@ -1,0 +1,7 @@
+package com.epam.ta.reportportal.core.tms.db.entity;
+
+public class DataSetColumn {
+    private Long id;
+    private String key;
+    private String value;
+}

src/main/java/com/epam/ta/reportportal/core/tms/db/repository/TestFolderRepository.java
@@ -1,0 +1,29 @@
+package com.epam.ta.reportportal.core.tms.db.repository;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsTestFolder;
+import com.epam.ta.reportportal.dao.ReportPortalRepository;
+
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Andrei_Varabyeu
+ */
+@Repository
+public interface TestFolderRepository extends ReportPortalRepository<TmsTestFolder, Long> {
+    /**
+     * Find all folder for given project
+     *
+     * @param projectID ID of project
+     * @return found folders
+     */
+    List<TmsTestFolder> findAllByProjectId(long projectID);
+
+    /**
+     * Finds a folder by given ID and project ID
+     * @param id ID of folder
+     * @param projectId ID of project
+     * @return Test Folder
+     */
+    TmsTestFolder findByIdAndProjectId(long id, long projectId);
+}

src/main/java/com/epam/ta/reportportal/core/tms/mapper/ProductVersionMapper.java
@@ -1,0 +1,19 @@
+package com.epam.ta.reportportal.core.tms.mapper;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsProductVersion;
+import com.epam.ta.reportportal.core.tms.dto.ProductVersionRS;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductVersionMapper implements DtoMapper<TmsProductVersion, ProductVersionRS> {
+
+    @Override
+    public ProductVersionRS convert(TmsProductVersion productVersion) {
+        return new ProductVersionRS(productVersion.getId(),
+                productVersion.getVersion(),
+                productVersion.getDocumentation(),
+                productVersion.getTestPlans(),
+                productVersion.getMilestones());
+
+    }
+}

src/main/java/com/epam/ta/reportportal/core/tms/mapper/TestCaseMapper.java
@@ -1,0 +1,27 @@
+package com.epam.ta.reportportal.core.tms.mapper;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsTestCase;
+import com.epam.ta.reportportal.core.tms.dto.TestCaseRS;
+import com.epam.ta.reportportal.core.tms.dto.TestCaseVersionRS;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class TestCaseMapper implements DtoMapper<TmsTestCase, TestCaseRS> {
+
+    public TestCaseRS convert(final TmsTestCase testCase) {
+        return new TestCaseRS(testCase.getId(),
+                testCase.getName(),
+                testCase.getDescription(),
+                testCase.getVersions()
+                        .stream()
+                        .map(model -> new TestCaseVersionRS(model.getId(),
+                                model.getName(),
+                                model.isDefault(),
+                                model.isDraft(),
+                                null))
+                        .collect(Collectors.toSet()));
+    }
+
+}

src/main/java/com/epam/ta/reportportal/core/tms/mapper/TestFolderMapper.java
@@ -1,0 +1,13 @@
+package com.epam.ta.reportportal.core.tms.mapper;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsTestFolder;
+import com.epam.ta.reportportal.core.tms.dto.TestFolderRS;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestFolderMapper implements DtoMapper<TmsTestFolder, TestFolderRS> {
+
+    public TestFolderRS convert(final TmsTestFolder testFolder) {
+        return new TestFolderRS(testFolder.getId(), testFolder.getName(), testFolder.getDescription());
+    }
+}

src/main/java/com/epam/ta/reportportal/core/tms/service/ProductVersionServiceImpl.java
@@ -1,0 +1,76 @@
+package com.epam.ta.reportportal.core.tms.service;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsMilestone;
+import com.epam.ta.reportportal.core.tms.db.entity.TmsProductVersion;
+import com.epam.ta.reportportal.core.tms.db.entity.TmsTestPlan;
+import com.epam.ta.reportportal.core.tms.db.repository.ProductVersionRepository;
+import com.epam.ta.reportportal.core.tms.dto.ProductVersionRQ;
+import com.epam.ta.reportportal.core.tms.dto.ProductVersionRS;
+import com.epam.ta.reportportal.core.tms.exception.NotFoundException;
+import com.epam.ta.reportportal.core.tms.mapper.DtoMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductVersionServiceImpl implements ProductVersionService {
+
+    private static final String VERSION_NOT_FOUND_BY_ID = "Product Version cannot be found by id: {0}";
+
+    private final DtoMapper<TmsProductVersion, ProductVersionRS> productVersionMapper;
+    private final ProductVersionRepository productVersionRepository;
+
+    @Override
+    public ProductVersionRS create(long projectID, final ProductVersionRQ inputDto) {
+//        final var testSuite = testSuiteRepository.findById(inputDto.testFolderId())
+//                .orElseThrow(NotFoundException.supplier(TEST_SUITE_NOT_FOUND_BY_ID, inputDto.testFolderId())); // replace by getting default Test Suite
+        final var productVersion = new TmsProductVersion(null,
+                inputDto.version(),
+                inputDto.documentation(),
+                inputDto.testPlans().stream().map(it -> {
+                  var tp = new TmsTestPlan();
+                  tp.setId(it);
+                  return tp;
+                }).collect(Collectors.toSet()),
+                inputDto.milestones().stream().map(it -> {
+                    var milestone = new TmsMilestone();
+                    milestone.setId(it);
+                    return milestone;
+                }).collect(Collectors.toSet()));
+
+        return productVersionMapper.convert(productVersionRepository.save(productVersion));
+    }
+
+    @Override
+    public ProductVersionRS update(long projectID,final Long productVersionID, final ProductVersionRQ inputDto) {
+        final var productVersion = new TmsProductVersion(productVersionID,
+                inputDto.version(),
+                inputDto.documentation(),
+                inputDto.testPlans().stream().map(it -> {
+                    var tp = new TmsTestPlan();
+                    tp.setId(it);
+                    return tp;
+                }).collect(Collectors.toSet()),
+                inputDto.milestones().stream().map(it -> {
+                    var milestone = new TmsMilestone();
+                    milestone.setId(it);
+                    return milestone;
+                }).collect(Collectors.toSet()));
+
+        return productVersionMapper.convert(productVersionRepository.save(productVersion));    }
+
+    @Override
+    public void delete(long projectID,Long id) {
+        productVersionRepository.deleteById(id);
+    }
+
+    @Override
+    public ProductVersionRS getById(long projectID, Long id) {
+        return productVersionMapper.convert(productVersionRepository.findById(id)
+                .orElseThrow(NotFoundException.supplier(VERSION_NOT_FOUND_BY_ID, id)));
+    }
+
+
+}

src/main/java/com/epam/ta/reportportal/core/tms/service/TestCaseServiceImpl.java
@@ -1,0 +1,61 @@
+package com.epam.ta.reportportal.core.tms.service;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsTestCase;
+import com.epam.ta.reportportal.core.tms.db.entity.TmsTestCaseVersion;
+import com.epam.ta.reportportal.core.tms.db.repository.TestCaseRepository;
+import com.epam.ta.reportportal.core.tms.db.repository.TestFolderRepository;
+import com.epam.ta.reportportal.core.tms.dto.TestCaseRQ;
+import com.epam.ta.reportportal.core.tms.dto.TestCaseRS;
+import com.epam.ta.reportportal.core.tms.exception.NotFoundException;
+import com.epam.ta.reportportal.core.tms.mapper.DtoMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static com.epam.ta.reportportal.core.tms.service.TestFolderServiceImpl.TEST_FOLDER_NOT_FOUND_BY_ID;
+
+@Service
+@RequiredArgsConstructor
+public class TestCaseServiceImpl implements TestCaseService {
+
+    private static final String TEST_CASE_NOT_FOUND_BY_ID = "Test Case cannot be found by id: {0}";
+
+    private final DtoMapper<TmsTestCase, TestCaseRS> testCaseMapper;
+    private final TestCaseRepository testCaseRepository;
+    private final TestFolderRepository testFolderRepository;
+
+    @Override
+    public TestCaseRS createTestCase(final TestCaseRQ inputDto) {
+        final var testFolder = testFolderRepository.findById(inputDto.testFolderId())
+                .orElseThrow(NotFoundException.supplier(TEST_FOLDER_NOT_FOUND_BY_ID, inputDto.testFolderId())); // replace by getting default Test Folder
+        final var testCase = new TmsTestCase(null,
+                inputDto.name(),
+                inputDto.description(),
+                new HashSet<>(),
+                new HashSet<>(),
+                testFolder);
+        testCase.addTestCaseVersion(new TmsTestCaseVersion(null, "Default", true, false, null));
+
+        return testCaseMapper.convert(testCaseRepository.save(testCase));
+    }
+
+    @Override
+    public TestCaseRS updateTestCase(final long testCaseId, final TestCaseRQ inputDto) {
+        final var testCase = testCaseRepository.findById(testCaseId)
+                .orElseThrow(NotFoundException.supplier(TEST_FOLDER_NOT_FOUND_BY_ID, testCaseId)); // replace by getting default Test Folder
+        return null;
+    }
+
+    @Override
+    public TestCaseRS getTestCaseById(long projectId, long id) {
+        return testCaseMapper.convert(testCaseRepository.findById(id)
+                .orElseThrow(NotFoundException.supplier(TEST_CASE_NOT_FOUND_BY_ID, id)));
+    }
+    @Override
+    public List<TestCaseRS> getTestCaseByProjectId(long projectId) {
+        return testCaseRepository.findByTestFolder_ProjectId(projectId).stream().map(testCaseMapper::convert).toList();
+    }
+
+}